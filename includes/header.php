82489de7dfe4240e8888ae9d56e7bd5418484c67
Anuj Kumar <anujkumar@Anujs-Mac-mini.local>
2026-02-01 01:04:29 +0530

    Branding: Added custom favicon to the application.

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SecureVault</title>
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
